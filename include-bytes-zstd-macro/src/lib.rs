e831225b3700969f368311e400c28f31137b6336
Koichi Akabe <vbkaisetsu@gmail.com>
2023-02-17 17:38:54 +0900

    Use CARGO_MANIFEST_DIR

--- include-bytes-zstd-macro/src/lib.rs
+++ include-bytes-zstd-macro/src/lib.rs
@@ -2,9 +2,11 @@
 
 extern crate proc_macro;
 
+use std::io::Write;
+use std::path::PathBuf;
+
 use proc_macro::TokenStream;
 use proc_macro_crate::FoundCrate;
-use std::io::Write;
 use syn::{parse::Parse, parse_macro_input, LitInt, LitStr, Token};
 
 struct Arguments {
@@ -31,16 +33,27 @@ impl Parse for Arguments {
 /// * `level`: Compression level (1-21).
 #[proc_macro]
 pub fn include_bytes_zstd(input: TokenStream) -> TokenStream {
+    // parses arguments
     let args = parse_macro_input!(input as Arguments);
-    let filename = args.filename.value();
+    let filename = PathBuf::from(args.filename.value());
     let level = args.level.base10_parse::<i32>().unwrap();
-    let bytes = std::fs::read(filename).unwrap();
 
+    // resolves file path
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let filename = if filename.is_relative() {
+        manifest_dir.join(filename)
+    } else {
+        filename
+    };
+
+    // compression
+    let bytes = std::fs::read(filename).unwrap();
     let mut compressed = vec![];
     let mut encoder = zstd::Encoder::new(&mut compressed, level).unwrap();
     encoder.write_all(&bytes).unwrap();
     encoder.finish().unwrap();
 
+    // generates code
     let crate_name = proc_macro_crate::crate_name("include-bytes-zstd").unwrap();
     let crate_name = match crate_name {
         FoundCrate::Itself => "include_bytes_zstd".to_string(),
